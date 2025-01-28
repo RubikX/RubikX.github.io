caddb1b39eff24ee08847493789c96e1b2a750a5
RubikX <ohtehinternets@hotmail.com>
2025-01-27 20:04:53 -0800

    Remove secret

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = "";
 let standingsData = {};
 const modal = document.getElementById('boxscore-modal');
 const closeBtn = document.querySelector('.close');
